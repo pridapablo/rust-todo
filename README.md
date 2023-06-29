58fd6a10864141233c9cc99229f0353f65d953a3
Pablo Banzo <pridapablo@gmail.com>
2023-06-29 00:41:37 -0600

    add a task to todo done

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 This is my first Rust project. It's a simple command line tool for creating and managing a todo list.
 
 The code was written following [this](https://www.freecodecamp.org/news/how-to-build-a-to-do-app-with-rust/) tutorial.
+
+Note: in the initial implementation the todo list is stored in a file for long term persistence. The first extension to the project will be to use a database instead.
+
+## Usage
+
+### Add a task
+
+```rust
+$ cargo run add "Buy milk"
+```
+
+## Roadmap:
+
+- [x] Add a task
+- [ ] List tasks
+- [ ] Mark a task as complete
+- [ ] Delete a task
+- [ ] Store tasks in a file
+- [ ] Store tasks in a database
+- [ ] Add a due date to a task
+- [ ] Add a priority to a task
+- [ ] Add a project to a task
+- [ ] List tasks by due date
+- [ ] List tasks by priority
+- [ ] List tasks by project
+- [ ] Add a command to list all tasks
+- [ ] Add a command to list all tasks by due date
+- [ ] Add a command to list all tasks by priority
+- [ ] Add a command to list all tasks by project
+- [ ] Add a command to list all tasks by due date and priority
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,72 @@
 // Imports
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::format};
 
-// All variables are immutable by default
-// 
+// All variables are immutable by default We can opt to make it mutable by using
+// "mut"
 
 // Main function
 fn main() {
+    // CLI input binding code:
     let action = std::env::args().nth(1).expect("Please specify an action");
     // considering that std::env::args() is an iterator we use .nth to access
     // values .expect is a method for the Option enum that will return the value
     // or panic with that return msg
     let item = std::env::args().nth(2).expect("Please specify an item");
 
-    println!("{:?}, {:?}", action, item);
+    println!("Recieved action: {:?}, and item: {:?}", action, item);
+
+    // We instanciate the class and initialize the map. The todo instance is mutable
+    let mut todo = Todo{
+        map: HashMap::new(),
+    };
+
+    if action == "add" {
+        // we call the insert method
+        todo.insert(item);
+        // we match the result type returned from the save fn
+        match todo.save() {
+            Ok(_) => println!("Todo Saved"),
+            Err(why) => println!("An error occurred: {}", why),
+        }
+    }
 }
 
 // We can implement our own type for the todo list
 struct Todo {
-    // Using the built in HashMap for key-value pairs
-    // map is our field
+    // Using the built in HashMap for key-value pairs map is our field
     // HashMap<String, bool> means that keys will be strings and values booleans
     map: HashMap<String, bool>
 }
 
-// impl Todo to add methods to the struct
-// Their first parameter is ALWAYS self
+// impl Todo to add methods to the struct Their first parameter is ALWAYS self
 impl Todo {
-    // we declare a function (method)
+    // we declare a function (method) It takes two arguments, the first is self
+    // (a mutable pointer to the location where value is stored and the key)
+    // NOTE: this is a borrow since we don't "own" self (use &)
     fn insert(&mut self, key: String) {
-        // insert a new item into the hash map
-        // passing true as value
+        // insert a new item into the hash map passing true as value
         self.map.insert(key, true);
         // insert is a built in method
     }    
+
+    // We will write to a file to store todo's
+    fn save(self) -> Result<(), std::io::Error>
+    // Here, -> is for typing the return of the function (it returns a result for error handling)
+     {
+        let mut content = String::new();
+
+        for (k, v) in self.map {
+            // we iterate over the hash map and format each string, separating
+            // key and value with a tabl char (\t) and each line with a new line
+            // (\n)
+            let record = format!("{}\t{}\n",k,v);
+            content.push_str(&record)
+        }
+        // We persist the data in the db.txt file
+        std::fs::write("db.txt", content)
+    }
 }
+
+// The ownership system has three rules: Each value in Rust has a variable: its
+// owner. There can only be one owner at a time for each value. When the owner
+// goes out of scope, the value will be dropped.
